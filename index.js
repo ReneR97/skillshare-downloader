54c4bea0aead0955807612a474c7aee168b3cfa4
Remling97 <reiterer.rene97@gmail.com>
2023-04-23 22:07:13 +0200

    Added Subtitles Support

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { http, https } = require('follow-redirects');
-const fs = require('fs');
+import https from 'follow-redirects';
+import cloudflareScraper from 'cloudflare-scraper';
+import fs from 'fs';
 
 let classID = '';
 let phpsessid = '';
 let skillshare_user_ = '';
+
+let subtitles = ''; //en, de, pt...
 
 scrapeVideos();
 
@@ -19,21 +22,54 @@ async function scrapeVideos() {
     let downloaded = 0;
     let maxItems = classData['_embedded']['sessions']['_embedded']['sessions'].length;
 
-    classData['_embedded']['sessions']['_embedded']['sessions'].forEach(async (session) => {
+    classData['_embedded']['sessions']['_embedded']['sessions'].forEach(async (session, i) => {
         let dl = await download(session, dir);
 
         if (dl) {
             downloaded++;
             console.log(`Download ${downloaded}/${maxItems} Completed!`);
+
+            if (subtitles != '') {
+                let video_hashed_id;
+                if (i == 0) video_hashed_id = session['video_hashed_id'].replace('bc:', '');
+                else video_hashed_id = /thumbnails\/(.*)\//gm.exec(session['image_thumbnail'])[1];
+
+                let subtitles_data = await fetchSubtitlesInfo(`https://edge.api.brightcove.com/playback/v1/accounts/3695997568001/videos/${video_hashed_id}`);
+
+                if (subtitles_data['error_code']) console.log('There was an error getting the subtitles data. ERROR: ' + subtitles_data);
+                else {
+                    let subtitles_url = '';
+
+                    subtitles_data['text_tracks'].forEach((el) => {
+                        if (el['srclang'].includes(subtitles)) subtitles_url = el['src'];
+                    });
+
+                    if (subtitles_url == '') console.log("Couldn't find subtitles for specified language");
+                    else {
+                        let dl = await downloadSubtitles(subtitles_url, dir, session);
+                        if (dl) console.log(`Download Subtitles ${downloaded}/${maxItems} Completed!`);
+                        else console.log('There was an Error when downloading the Subtitles');
+                    }
+                }
+            }
         } else {
             console.log('COOKIE ERROR');
         }
     });
 }
 
+async function downloadSubtitles(url, dir, session) {
+    const response = await cloudflareScraper.get(url);
+    const path = `${dir}${session['index']}_${convertToValidFilename(session['_links']['download']['title'])}.vtt`;
+
+    fs.writeFileSync(path, response.body);
+
+    return true;
+}
+
 function download(session, dir) {
     return new Promise((resolve, reject) => {
-        req = https.request(
+        let req = https.https.request(
             {
                 method: 'GET',
                 hostname: 'api.skillshare.com',
@@ -70,7 +106,22 @@ function download(session, dir) {
 async function fetchFromApi(apiURL) {
     const response = await fetch(apiURL, {
         method: 'get',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await response.json();
+
+    return data;
+}
+
+async function fetchSubtitlesInfo(apiURL) {
+    const response = await fetch(apiURL, {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json;pk=BCpkADawqM2OOcM6njnM7hf9EaK6lIFlqiXB0iWjqGWUQjU7R8965xUvIQNqdQbnDTLz0IAO7E6Ir2rIbXJtFdzrGtitoee0n1XXRliD-RH9A-svuvNW9qgo3Bh34HEZjXjG4Nml4iyz3KqF',
+            accept: 'application/json;pk=BCpkADawqM2OOcM6njnM7hf9EaK6lIFlqiXB0iWjqGWUQjU7R8965xUvIQNqdQbnDTLz0IAO7E6Ir2rIbXJtFdzrGtitoee0n1XXRliD-RH9A-svuvNW9qgo3Bh34HEZjXjG4Nml4iyz3KqF',
+        },
     });
     const data = await response.json();
 
